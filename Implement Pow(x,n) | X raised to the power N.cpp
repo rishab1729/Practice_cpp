ff47145d539f72a4937cd6828527f32bb19e9feb
Rishab Sharma <90676987+rishab1729@users.noreply.github.com>
2022-06-17 20:53:01 +0530

    Create Implement Pow(x,n) | X raised to the power N.cpp

---
+++ Implement Pow(x,n) | X raised to the power N.cpp
@@ -0,0 +1,31 @@
+/*
+Problem Statement 13: Given a double x and integer n, calculate x raised to power n. Basically Implement pow(x, n).
+
+Examples:
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+
+Output: 1024.00000
+
+Explanation: You need to calculate 2.00000 raised to 10 which gives ans 1024.00000
+
+Example 2:
+
+Input: x = 2.10000, n = 3
+
+Output: 9.26100
+
+Explanation: You need to calculate 2.10000 raised to 3 which gives ans 9.26100
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double power, double base) {
+        return pow(power,base);
+    }
+};
